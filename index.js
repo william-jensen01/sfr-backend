0ad4689ade2ee863af12b46ca1963c9c1d93b333
William Jensen <williambjensen01@gmail.com>
2021-02-08 20:19:21 -0700

    added environment port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require('./api/server');
 
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 server.listen(port, () => {
     console.log(`\n*** Server is running on port ${port} ***\n`)
