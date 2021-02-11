b665184fcc9eb62e728fb13e692665c879ea5464
William Jensen <williambjensen01@gmail.com>
2021-02-10 17:11:40 -0700

    added cors

--- api/server.js
+++ api/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const RecipesRouter = require('./recipes/router');
 
 const server = express();
 
+server.use(cors())
 server.use(express.json());
 server.use('/api/recipes', RecipesRouter)
 
